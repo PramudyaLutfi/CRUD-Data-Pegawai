7113217570b9a702cc00679f7aacd186ab1be2c1
PramudyaLutfi <lutfipramudya97@gmail.com>
2023-01-12 15:29:43 +0700

    sweet alert delete confirm

--- resources/views/datapegawai.blade.php
+++ resources/views/datapegawai.blade.php
@@ -7,6 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
 
     <title>CRUD LARAVEL8</title>
   </head>
@@ -49,7 +50,7 @@
                                 <td>{{$row->created_at->format('D M Y')}}</td>
                                 <td>
                                     <a href="/tampilkandata/{{ $row->id }}" class="btn btn-info">edit</a>
-                                    <a href="/delete/{{ $row->id }}" class="btn btn-danger">Delete</a>
+                                    <a href="#" class="btn btn-danger delete"data-id="{{$row->id}}" data-nama="{{$row->nama}}">Delete</a>
                                 </td>
                             </tr>
                     @endforeach
@@ -66,5 +67,38 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script> -->
+
   </body>
+<script>
+    $('.delete').click(function(){
+        var pegawaiid = $(this).attr('data-id');
+        var nama = $(this).attr('data-nama');
+
+        swal({
+        title: "Yakin?",
+        text: "Kamu akan menghapus data pegawai dengan nama "+nama+" ",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+        })
+        .then((willDelete) => {
+        if (willDelete) {
+            window.location = "/delete/"+pegawaiid+""
+            swal("Data berhasil di hapus", {
+            icon: "success",
+            });
+        } else {
+            swal("Data tidak jadi dihapus");
+        }
+        });
+    });
+
+     
+</script>
+
+<!-- <script>
+    toastr.success('Data has been saved successfully!', 'Congrats')
+</script> -->
 </html>
